a9524ce31a941f9aef74a4eaeec4bbedaced049f
Tjay <TjayG@protonmail.com>
2021-01-01 17:14:49 +0100

    Add YoutubeDownloader as the possiblity to downloada youtube video as mp3 with highest bitrate

--- src/main/java/com/senseikiri/lono/Main.java
+++ src/main/java/com/senseikiri/lono/Main.java
@@ -1,8 +1,12 @@
 package com.senseikiri.lono;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test");
+        YoutubeDownloadService service = new YoutubeDownloadService();
+        service.downloadYoutubeVideo("6hhjTUImkmY");
     }
 }
