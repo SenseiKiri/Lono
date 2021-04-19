c6d33f071106aeec69de17298662927806597c1d
Tjay <TjayG@protonmail.com>
2021-04-19 21:52:07 +0200

    Add typescript react with input and button

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <input defaultValue="Test"/>
+            <p/>
+            <button>Accept</button>
+        </div>
+    );
+}
+
+export default App;
